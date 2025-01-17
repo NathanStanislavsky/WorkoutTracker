38dea802556c7f4efb26c935cf4ea5c21a7b5624
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-16 22:31:40 -0500

    green test

---
+++ WorkoutTracker/src/lib/navbar/navbar.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<nav class="bg-gray-700 p-4 text-white">
+  <div class="container mx-auto flex items-center justify-between">
+    <div class="flex items-center">
+      <span class="font-bold text-xl">Workout Tracker</span>
+    </div>
+  </div>
+</nav>

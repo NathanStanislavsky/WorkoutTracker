3aab60165dab38f7e5a5db518ed2bbed657f5381
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-19 21:31:24 -0500

    green test

--- WorkoutTracker/src/routes/Signup/+page.svelte
+++ WorkoutTracker/src/routes/Signup/+page.svelte
@@ -18,6 +18,33 @@
       />
     </div>
     <div>
+      <label for="weight" class="block text-sm font-medium text-gray-700"
+        >Weight</label
+      >
+      <input
+        type="number"
+        id="weight"
+        name="weight"
+        required
+        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        placeholder="Enter your weight"
+      />
+    </div>
+
+    <div>
+      <label for="age" class="block text-sm font-medium text-gray-700"
+        >Age</label
+      >
+      <input
+        type="number"
+        id="age"
+        name="age"
+        required
+        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        placeholder="Enter your age"
+      />
+    </div>
+    <div>
       <label for="email" class="block text-sm font-medium text-gray-700"
         >Email</label
       >

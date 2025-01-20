48d7714118277a4f41be5267982c4b085842bf0d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-20 16:22:54 -0500

    green test

--- WorkoutTracker/src/routes/signin/+server.ts
+++ WorkoutTracker/src/routes/signin/+server.ts
@@ -1,11 +1,19 @@
 import type { RequestHandler } from './$types';
 import { prisma } from '$lib/server/prisma';
+import bcrypt from 'bcryptjs';
 
 export const POST: RequestHandler = async ({ request }: { request: Request }) => {
     const { email, password } = await request.json();
 
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
+      return new Response(JSON.stringify({ error: 'Invalid credentials' }), {
+        status: 401
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isPasswordValid) {
       return new Response(JSON.stringify({ error: 'Invalid credentials' }), {
         status: 401
       });

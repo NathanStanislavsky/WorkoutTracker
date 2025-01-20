802156f5d281dff8a27c353f05eeacf24bcfbeae
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-20 13:28:00 -0500

    red test

--- WorkoutTracker/src/routes/signin/signinPageServer.test.ts
+++ WorkoutTracker/src/routes/signin/signinPageServer.test.ts
@@ -12,12 +12,37 @@ describe("POST /login endpoint", () => {
 
     const reqBody = JSON.stringify({
       email: "nonexistent@example.com",
-      password: "password"
+      password: "password",
     });
 
     const request = new Request("http://localhost/signin", {
       method: "POST",
-      body: reqBody
+      body: reqBody,
+    });
+
+    const response = await POST({ request });
+    expect(response.status).toBe(401);
+
+    const data = await response.json();
+    expect(data).toHaveProperty("error", "Invalid credentials");
+  });
+
+  it("returns error if password does not match credentials", async () => {
+    const existingUser = {
+      email: "test@example.com",
+      passwordHash: "$2b$10$DummyHashForTestingPurposesOnly"
+    };
+
+    vi.spyOn(prisma.user, "findUnique").mockResolvedValue(existingUser);
+
+    const reqBody = JSON.stringify({
+      email: "test@example.com",
+      password: "wrong-password",
+    });
+
+    const request = new Request("http://localhost/signin", {
+      method: "POST",
+      body: reqBody,
     });
 
     const response = await POST({ request });

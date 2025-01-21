81a24c31d40862f8ca9c177c6d6a7e2424058efa
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-21 16:09:33 -0500

    created workout route

---
+++ WorkoutTracker/src/routes/workout/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import WorkoutItem from '$lib/workout card/WorkoutItem.svelte';
+  
+    const workouts = [
+      {
+        date: '2025-01-20',
+        workoutType: 'Cardio',
+        duration: 30,
+        calories: 250
+      },
+      {
+        date: '2025-01-21',
+        workoutType: 'Strength',
+        duration: 45,
+        calories: 300
+      }
+    ];
+  </script>
+  
+  <section class="max-w-2xl mx-auto mt-8 space-y-4">
+    {#each workouts as workout}
+      <WorkoutItem 
+        date={workout.date}
+        workoutType={workout.workoutType}
+        duration={workout.duration}
+        calories={workout.calories}
+      />
+    {/each}
+  </section>

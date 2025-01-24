51e5cadea08a291e3af717e902df98de5019c349
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-24 13:54:32 -0500

    green test

--- src/lib/updateWorkouts/addWorkout.svelte
+++ src/lib/updateWorkouts/addWorkout.svelte
@@ -1,8 +1,55 @@
 <script lang="ts">
 </script>
 
-<form class="max-w-md mx-auto p-6 bg-white shadow-md rounded">
+<form class="max-w-md mx-auto p-6 bg-slate-800 shadow-md rounded text-white">
   <h2 class="text-2xl mb-4">Add a New Workout</h2>
 
-  
+  <div class="mb-4">
+    <label for="workout-type" class="block mb-2">Type</label>
+    <select id="workout-type" class="w-full px-3 py-2 border rounded">
+      <option value="cardio">Cardio</option>
+      <option value="strength">Strength</option>
+      <option value="flexibility">Flexibility</option>
+      <option value="balance">Balance</option>
+    </select>
+  </div>
+
+  <div class="mb-4">
+    <label for="workout-date" class="block mb-2">Date</label>
+    <input
+      type="date"
+      id="workout-date"
+      required
+      class="w-full px-3 py-2 border rounded"
+    />
+  </div>
+
+  <div class="mb-4">
+    <label for="workout-duration" class="block mb-2">Duration (minutes)</label>
+    <input
+      type="number"
+      id="workout-duration"
+      required
+      min="1"
+      class="w-full px-3 py-2 border rounded"
+    />
+  </div>
+
+  <div class="mb-4">
+    <label for="calories-burned" class="blockmb-2">Calories Burned</label>
+    <input
+      type="number"
+      id="calories-burned"
+      required
+      min="1"
+      class="w-full px-3 py-2 border rounded"
+    />
+  </div>
+
+  <button
+    type="submit"
+    class="w-full bg-blue-500 py-2 rounded hover:bg-blue-600"
+  >
+    Add Workout
+  </button>
 </form>

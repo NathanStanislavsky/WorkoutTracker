9e8bc10d5a23a8fc3a8ad5ff28ac73975f0a6253
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-23 21:41:36 -0500

    green test

--- src/lib/navbar/navbar.svelte
+++ src/lib/navbar/navbar.svelte
@@ -20,6 +20,7 @@
           Home
         </button>
       </a>
+
       <!-- Sign up button -->
       <a href="/signup" class="p-4">
         <button
@@ -27,6 +28,16 @@
            px-5 py-2 rounded-md text-white text-2xl font-medium"
         >
           Sign Up
+        </button>
+      </a>
+
+      <!-- Sign in button -->
+      <a href="/signin" class="p-4">
+        <button
+          class="bg-slate-700 hover:bg-slate-600 transition-colors
+           px-5 py-2 rounded-md text-white text-2xl font-medium"
+        >
+          Sign In
         </button>
       </a>
     </div>

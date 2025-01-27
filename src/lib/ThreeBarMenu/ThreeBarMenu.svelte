a5db224e28bd01b70da2ee235106501a8dc83eba
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-27 10:58:02 -0500

    green test

---
+++ src/lib/ThreeBarMenu/ThreeBarMenu.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="relative inline-block text-left">
+  <button
+    class="flex flex-col items-center justify-center w-8 h-8 space-y-1 focus:outline-none"
+    aria-label="Toggle menu"
+  >
+    <span class="block w-6 h-0.5 bg-white"></span>
+    <span class="block w-6 h-0.5 bg-white"></span>
+    <span class="block w-6 h-0.5 bg-white"></span>
+  </button>
+</div>

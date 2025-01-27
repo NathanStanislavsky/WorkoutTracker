bd915514b2c06954c56be1c89ef117bd4ec5049d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-27 11:04:51 -0500

    green test

--- src/lib/ThreeBarMenu/ThreeBarMenu.svelte
+++ src/lib/ThreeBarMenu/ThreeBarMenu.svelte
@@ -1,39 +1,44 @@
 <script>
-    let isOpen = false;
-  
-    function toggleMenu() {
-      isOpen = !isOpen;
-    }
-  </script>
-  
-  <div class="relative inline-block text-left">
-    <button 
-      on:click={toggleMenu}
-      class="flex flex-col items-center justify-center w-8 h-8 space-y-1 focus:outline-none"
-      aria-label="Toggle menu"
+  let isOpen = false;
+
+  function toggleMenu() {
+    isOpen = !isOpen;
+  }
+
+  function handleEdit() {
+    alert("Edit action");
+  }
+</script>
+
+<div class="relative inline-block text-left">
+  <button
+    on:click={toggleMenu}
+    class="flex flex-col items-center justify-center w-8 h-8 space-y-1 focus:outline-none"
+    aria-label="Toggle menu"
+  >
+    <span class="block w-6 h-0.5 bg-white"></span>
+    <span class="block w-6 h-0.5 bg-white"></span>
+    <span class="block w-6 h-0.5 bg-white"></span>
+  </button>
+
+  {#if isOpen}
+    <div
+      class="absolute right-0 mt-2 w-36 bg-slate-700 border border-gray-200 rounded shadow-md"
+      role="menu"
     >
-      <span class="block w-6 h-0.5 bg-white"></span>
-      <span class="block w-6 h-0.5 bg-white"></span>
-      <span class="block w-6 h-0.5 bg-white"></span>
-    </button>
-  
-    {#if isOpen}
-      <div 
-        class="absolute right-0 mt-2 w-36 bg-slate-700 border border-gray-200 rounded shadow-md"
-        role="menu"
+      <button
+        on:click={handleEdit}
+        class="block w-full px-4 py-2 text-left hover:bg-slate-500 focus:outline-none"
+        role="menuitem"
       >
-        <button 
-          class="block w-full px-4 py-2 text-left hover:bg-slate-500 focus:outline-none"
-          role="menuitem"
-        >
-          Edit
-        </button>
-        <button 
-          class="block w-full px-4 py-2 text-left hover:bg-slate-500 focus:outline-none text-white"
-          role="menuitem"
-        >
-          Delete
-        </button>
-      </div>
-    {/if}
-  </div>
+        Edit
+      </button>
+      <button
+        class="block w-full px-4 py-2 text-left hover:bg-slate-500 focus:outline-none text-white"
+        role="menuitem"
+      >
+        Delete
+      </button>
+    </div>
+  {/if}
+</div>

b2cf5cc5c70e20eae2b11eaedaa91c74df0c0318
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-27 14:23:52 -0500

    hover effect on menu

--- src/lib/ThreeBarMenu/ThreeBarMenu.svelte
+++ src/lib/ThreeBarMenu/ThreeBarMenu.svelte
@@ -33,7 +33,7 @@
 <div class="relative inline-block text-left">
   <button
     on:click={toggleMenu}
-    class="flex flex-col items-center justify-center w-8 h-8 space-y-1 focus:outline-none"
+    class="flex flex-col items-center justify-center w-8 h-8 space-y-1 focus:outline-none hover:bg-slate-700"
     aria-label="Toggle menu"
   >
     <span class="block w-6 h-0.5 bg-white"></span>

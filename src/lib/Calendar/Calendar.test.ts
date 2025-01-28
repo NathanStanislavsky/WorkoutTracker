50c5d288e4d85b5d00036208a803e4d3f1f4245c
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-27 22:34:20 -0500

    red test

--- src/lib/Calendar/Calendar.test.ts
+++ src/lib/Calendar/Calendar.test.ts
@@ -15,12 +15,22 @@ describe("Calendar component", () => {
     vi.useRealTimers();
   });
 
-  it("renders the calendar component with December 2024", () => {
+  it("Renders calendar component with December 2024", () => {
     render(Calendar);
 
     const currentMonth = "December";
     const currentYear = "2024";
 
     expect(screen.getByText(`${currentMonth} ${currentYear}`)).toBeInTheDocument();
+  });
+
+  it("Renders calendar component with days of the week", () => {
+    render(Calendar);
+
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    days.forEach((day) => {
+      expect(screen.getByText(day)).toBeInTheDocument();
+    });
   });
 });

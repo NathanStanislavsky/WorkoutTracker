35e646eac73ac5d2d10692c770101dc624df433b
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-01-31 18:30:35 -0500

    refactor

--- src/lib/Exercise/Exercise.svelte
+++ src/lib/Exercise/Exercise.svelte
@@ -5,11 +5,9 @@
   export let weight: number;
 </script>
 
-<div
-  class="p-5 bg-slate-800 rounded-lg shadow-md hover:shadow-xl transition-shadow text-white"
->
-  <div class="flex items-center justify-between mb-4">
-    <h2 class="text-lg font-semibold">{name}</h2>
+<div class="p-5 bg-slate-800 rounded-lg shadow-md hover:shadow-xl transition-shadow text-white">
+  <div class="grid grid-cols-4 items-center gap-4 mb-4">
+    <h2 class="text-lg font-semibold truncate">{name}</h2>
 
     <div class="flex flex-col items-center">
       <p class="text-sm text-gray-300">Sets</p>
@@ -20,7 +18,7 @@
       <p class="text-sm text-gray-300">Reps</p>
       <p class="text-lg font-bold">{reps}</p>
     </div>
-
+    
     <div class="flex flex-col items-center">
       <p class="text-sm text-gray-300">Weight</p>
       <p class="text-lg font-bold">{weight}</p>

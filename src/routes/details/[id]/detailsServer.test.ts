ef770e44a6be1458a7e9243d2f69e5056c51feed
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-02 18:15:37 -0500

    red test

--- src/routes/details/[id]/detailsServer.test.ts
+++ src/routes/details/[id]/detailsServer.test.ts
@@ -2,6 +2,17 @@ import { describe, it, expect, vi, beforeEach } from "vitest";
 import { prisma } from "$lib/server/prisma";
 import { load } from "./+page.server";
 
+vi.mock("$lib/Server/prisma.ts", () => ({
+  prisma: {
+    workout: {
+      findFirst: vi.fn(),
+    },
+    exercise: {
+      findMany: vi.fn(),
+    },
+  },
+}));
+
 describe("Exercise loader", () => {
   beforeEach(() => {
     vi.resetAllMocks();
@@ -12,6 +23,15 @@ describe("Exercise loader", () => {
 
     const result = await load({ locals });
 
-    expect(result).toEqual({ workouts: [] });
+    expect(result).toEqual({ exercises: [] });
+  });
+
+  it("if workout does not exist throw error", async () => {
+    const locals = { user: { id: 1 } };
+    const params = { workoutId: "non-existent-id" };
+
+    prisma.workout.findFirst.mockResolvedValue(null);
+
+    await expect(load({ locals, params })).rejects.toThrow("Workout not found");
   });
 });

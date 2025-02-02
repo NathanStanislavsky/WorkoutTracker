af3b717b82344c9512e999518419a7271af30f81
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-02 18:15:44 -0500

    green test

--- src/routes/details/[id]/+page.server.ts
+++ src/routes/details/[id]/+page.server.ts
@@ -1,11 +1,28 @@
+import { prisma } from "$lib/Server/prisma.ts";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, params }) => {
   const user = locals.user;
   
   if (!user) {
-    return {
-      workouts: [],
-    };
+    return { exercises: [] };
+  }
+  
+  const { workoutId } = params;
+  
+  try {
+    const workout = await prisma.workout.findFirst({
+      where: {
+        id: workoutId,
+        userId: user.id,
+      },
+    });
+    
+    if (!workout) {
+      throw new Error("Workout not found");
+    }
+  } catch (error) {
+    console.error("Error fetching exercises:", error);
+    throw error;
   }
 };

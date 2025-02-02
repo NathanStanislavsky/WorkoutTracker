268a43d0afe2018d4daba25b2c96973b20a82a63
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-02 17:45:15 -0500

    changed styling for detail page

--- src/routes/details/[id]/+page.svelte
+++ src/routes/details/[id]/+page.svelte
@@ -2,18 +2,20 @@
   import ExerciseParent from "$lib/Exercise-Parent/Exercise-Parent.svelte";
 </script>
 
-<span class="p-4">
-  <ExerciseParent
-    exercises={[
-      { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
-      { name: "Squats", sets: 3, reps: 10, weight: 225 },
-      { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
-      { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
-      { name: "Squats", sets: 3, reps: 10, weight: 225 },
-      { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
-      { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
-      { name: "Squats", sets: 3, reps: 10, weight: 225 },
-      { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
-    ]}
-  />
-</span>
+<div class="min-h-screen flex items-center justify-center">
+  <div class="w-full max-w-xl h-[75vh]">
+    <ExerciseParent
+      exercises={[
+        { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
+        { name: "Squats", sets: 3, reps: 10, weight: 225 },
+        { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
+        { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
+        { name: "Squats", sets: 3, reps: 10, weight: 225 },
+        { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
+        { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
+        { name: "Squats", sets: 3, reps: 10, weight: 225 },
+        { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
+      ]}
+    />
+  </div>
+</div>

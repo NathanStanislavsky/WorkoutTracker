44cc14c35b3faac99cd16c1f8f2d42aa4aceff10
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-03 09:57:47 -0500

    refactor

--- src/routes/details/[id]/+page.svelte
+++ src/routes/details/[id]/+page.svelte
@@ -1,21 +1,26 @@
-<script>
+<script lang="ts">
   import ExerciseParent from "$lib/Exercise-Parent/Exercise-Parent.svelte";
+
+  import type { PageData } from "./$types";
+  export let data: PageData;
+
+  const exercises = data.exercises as Array<{
+    id: number;
+    name: string;
+    sets: number;
+    reps: number;
+    weight: number;
+  }>;
+
+
 </script>
 
 <div class="min-h-screen flex items-center justify-center">
   <div class="w-full max-w-xl h-[75vh]">
-    <ExerciseParent
-      exercises={[
-        { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
-        { name: "Squats", sets: 3, reps: 10, weight: 225 },
-        { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
-        { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
-        { name: "Squats", sets: 3, reps: 10, weight: 225 },
-        { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
-        { name: "Bench Press", sets: 3, reps: 10, weight: 135 },
-        { name: "Squats", sets: 3, reps: 10, weight: 225 },
-        { name: "Deadlifts", sets: 3, reps: 10, weight: 315 },
-      ]}
-    />
+    {#if exercises.length > 0}
+      <ExerciseParent {exercises} />
+    {:else}
+      <p class="text-center text-gray-500">No exercises found</p>
+    {/if}
   </div>
 </div>

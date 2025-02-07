0fcb1d747d8600ae5938d64b39f33dcea93525d5
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-07 16:14:36 -0500

    green test

--- src/routes/details/deleteExercise/[id]/+server.ts
+++ src/routes/details/deleteExercise/[id]/+server.ts
@@ -1,10 +1,13 @@
 import type { RequestHandler } from "./$types";
 import { error } from "@sveltejs/kit";
 
-export const DELETE: RequestHandler = async ({locals }) => {
+export const DELETE: RequestHandler = async ({ params, locals }) => {
   if (!locals.user) {
     throw error(401, "Unauthorized");
   }
 
-  
+  const exerciseId = Number(params.id);
+  if (!exerciseId) {
+    throw error(400, "Invalid exercise ID");
+  }
 };

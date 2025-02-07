aa10a4033b77aa279ed9658993100c58ae3c5512
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-07 16:14:33 -0500

    red test

--- src/routes/details/deleteExercise/[id]/deleteExerciseServer.test.ts
+++ src/routes/details/deleteExercise/[id]/deleteExerciseServer.test.ts
@@ -8,10 +8,21 @@ describe("delete exercise endpoint tests", () => {
 
   it("should check if user jwt exists (user is logged in)", async () => {
     const locals = { user: null };
+    const params = { id: '1' };
 
-    await expect(DELETE({ locals } as any)).rejects.toMatchObject({
+    await expect(DELETE({ params, locals } as any)).rejects.toMatchObject({
       status: 401,
       body: { message: "Unauthorized" }
+    });
+  });
+
+  it('should check if exercise ID is valid', async () => {
+    const locals = { user: { id: 1 }};
+    const params = { id: '' };
+
+    await expect(DELETE({ params, locals } as any)).rejects.toMatchObject({
+      status: 400,
+      body: { message: 'Invalid exercise ID' }
     });
   });
 });

6825908a06dc01baa8d0fb31e2fb5a824ffa074b
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-07 16:06:21 -0500

    green test

---
+++ src/routes/details/[id]/deleteExercise/[id]/+server.ts
@@ -0,0 +1,8 @@
+import type { RequestHandler } from "./$types";
+import { error } from "@sveltejs/kit";
+
+export const DELETE: RequestHandler = async ({locals }) => {
+  if (!locals.user) {
+    throw error(401, "Unauthorized");
+  }
+};

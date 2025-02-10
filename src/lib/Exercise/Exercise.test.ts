49e1da9d83de3851b9f84b882fd4b10847f15437
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-10 11:41:07 -0500

    refactor

--- src/lib/Exercise/Exercise.test.ts
+++ src/lib/Exercise/Exercise.test.ts
@@ -3,32 +3,30 @@ import { render } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 import Exercise from './Exercise.svelte';
 
+const defaultProps = {
+  name: 'Push Ups',
+  sets: 3,
+  reps: 10,
+  weight: 100,
+};
+
+const setup = (props = defaultProps) => render(Exercise, { props });
+
 describe('Exercise Component', () => {
-    it('renders exercise details correctly', () => {
-        const { getByText } = render(Exercise, { props: {
-            name: 'Push Ups',
-            sets: 3,
-            reps: 10,
-            weight: 100,
-        }});
+  it('renders exercise details correctly', () => {
+    const { getByText } = setup();
 
-        expect(getByText('Push Ups')).toBeInTheDocument();
-        expect(getByText('Sets')).toBeInTheDocument();
-        expect(getByText('3')).toBeInTheDocument();
-        expect(getByText('Reps')).toBeInTheDocument();
-        expect(getByText('10')).toBeInTheDocument();
-        expect(getByText('Weight')).toBeInTheDocument();
-        expect(getByText('100')).toBeInTheDocument();
-    });
+    expect(getByText('Push Ups')).toBeInTheDocument();
+    expect(getByText('Sets')).toBeInTheDocument();
+    expect(getByText('3')).toBeInTheDocument();
+    expect(getByText('Reps')).toBeInTheDocument();
+    expect(getByText('10')).toBeInTheDocument();
+    expect(getByText('Weight')).toBeInTheDocument();
+    expect(getByText('100')).toBeInTheDocument();
+  });
 
-    it('renders exercise component with "delete" button', () => {
-        const { getByText } = render(Exercise, { props: {
-            name: 'Push Ups',
-            sets: 3,
-            reps: 10,
-            weight: 100,
-        }});
-
-        expect(getByText('x')).toBeInTheDocument();
-    });
+  it('renders a delete button', () => {
+    const { getByText } = setup();
+    expect(getByText('x')).toBeInTheDocument();
+  });
 });

1dcfa73a0c4c7cb4758aa38af49f053a7a743951
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-11 17:18:29 -0500

    green test

---
+++ src/routes/details/editExercise/[id]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="h-full flex items-center justify-center">
+  <form
+    class="w-full max-w-md p-6 bg-slate-800 shadow-md rounded-md"
+  >
+    <h2 class="text-2xl mb-4 text-white">Edit Exercise</h2>
+  </form>
+</div>
